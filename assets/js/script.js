c84168c80e6acbb6b3ad2bf45091576958055d0f
IkonicResonance <67353747+IkonicResonance@users.noreply.github.com>
2023-05-24 05:10:54 -0500

    Added question and choice population via jquery and bootstrap

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,32 +9,49 @@ quizPrompts = [
     "Arrays in JavaScript can be used to store _____.",
     "String values must be enclosed within _____ when being assigned to variables.",
     "A very useful tool used during development and debugging for printing content to the debugger is:"
-]
+    ]
+choices = [q1Choices, q2Choices, q3Choices, q4Choices, q5Choices]
 gameTime = 75
 currentQ = 0
 quizAnswers = [2, 2, 3, 2, 3]
+timerText = $("time").text
+//Function to for loop over items and append them to list
+function loadQuestion(){
+    //Set the current question's p text...
+    $("p").text(quizPrompts[currentQ])
+    //...and align it to center
+    $("p").attr("style", "text-align: center")
+    //For loop over choices and create/append them one by one to the list with bootstrap buttons.
+    for (index = 0; index < 4; index++)
+        $("ol").append("<li class='btn btn-info m-1'>" + choices[currentQ][index] + "</li>")
+}
 //Handler Function to be called onclick of start
-function quizHandler (){
-    console.log("feck")
+function quizHandler(){
     //Remove h1 from html
     $("h1").remove()
     //Remove button from html
     $("#start-quiz").remove()
     //Create ol and append to the main content
-    $("main").append("<ol>")
-    console.log(document)
-    //For loop over choices and create/append them one by one to the list with bootstrap.
+    $("main").append("<ol class='d-flex flex-column'></ol>")
+    //Call the loadQuestion function
+    loadQuestion()
     //Event Handler for onclick of the user's choice
         //Get the text of the clicked answer
         //Add and append a div 
         //add and append to div a border line
-        //Check if the answer is correct and if so, add/append to div "Correct" and if the currentQ++ is > 4 break the timer, otherwise go to next Question
+        //Check if the answer is correct and if so, add/append to div "Correct", otherwise go to next Question
         //If answer is wrong, Add/append to div "Wrong" and deduct time.
-    //Set time to 75
+        //Check if the currentQ++ is > 4 and break the timer if so, otherwise call the loadQuestion function
+    gameTime = 75
     //Start Timer
-            //Subtract from timer
-            //Update progress bar and label
-            //Check if time < 1 and if so break self
+    setInterval(function () {
+        //Subtract from timer
+        //Update progress bar and label
+        //Check if time < 1 and if so break self
+        gameTime--
+    }, 1000);
+
+            
     //When timer is broken, check the time left, and if it's more than 0, assign the time left to a temporary score
 
     //Prompt user to enter up to 4 initials or enter anonymously. If something is entered, save that string over the default "Sudo" name
