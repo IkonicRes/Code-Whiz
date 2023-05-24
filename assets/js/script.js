05c561a9f8a28360c3153848534d1843f26f0042
IkonicResonance <67353747+IkonicResonance@users.noreply.github.com>
2023-05-24 02:22:13 -0500

    Initial commit, most bones present.

---
+++ assets/js/script.js
@@ -0,0 +1,28 @@
+
+
+
+//Callback Function to allow user to start the code quiz
+
+    //Start Timer
+
+    //Present the user with a question
+
+    //Evaluate whether question was answered correctly, if so advance to the next question.
+        //If no more questions exist, break timer loop early
+
+    //If not, subtract time from timer
+
+
+//When timer is broken, check the time left, and if it's more than 0, assign the time left to a temporary score
+
+//Prompt user to enter up to 4 initials or enter anonymously. If something is entered, save that string over the default "Sudo" name
+
+//Save name and score to local storage score array
+
+//Reset all variables to default
+
+
+
+
+
+//Make event handler for "onclick" and call the above callback function
