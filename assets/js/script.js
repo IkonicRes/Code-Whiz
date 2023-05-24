7abe676d72c4e7b92ac22580318c2d566c25a69d
IkonicResonance <67353747+IkonicResonance@users.noreply.github.com>
2023-05-24 04:37:36 -0500

    set up the second page

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,25 +3,34 @@ q2Choices = ["quotes", "curly brackets", "parentheses", "square brackets"]
 q3Choices = ["numbers and strings", "other arrays", "booleans", "all of the above"]
 q4Choices = ["commas", "curly brackets", "quotes", "parentheses"]
 q5Choices = ["JavaScript", "terminal/bash", "for loops", "console.log"]
-questionPrompts = [
+quizPrompts = [
     "Commonly used data types DO NOT include:",
     "The condition in an if/else statement is enclosed within _____.",
     "Arrays in JavaScript can be used to store _____.",
-    "String values must be enclosed within _____ when being assigned to variables."
+    "String values must be enclosed within _____ when being assigned to variables.",
+    "A very useful tool used during development and debugging for printing content to the debugger is:"
 ]
 gameTime = 75
 currentQ = 0
-quizAnswers = [2, ]
+quizAnswers = [2, 2, 3, 2, 3]
 //Handler Function to be called onclick of start
+function quizHandler (){
+    console.log("feck")
+    //Remove h1 from html
+    $("h1").remove()
     //Remove button from html
-    //create ol and append
+    $("#start-quiz").remove()
+    //Create ol and append to the main content
+    $("main").append("<ol>")
+    console.log(document)
     //For loop over choices and create/append them one by one to the list with bootstrap.
     //Event Handler for onclick of the user's choice
         //Get the text of the clicked answer
         //Add and append a div 
-        //add and append to div a spacer line
+        //add and append to div a border line
         //Check if the answer is correct and if so, add/append to div "Correct" and if the currentQ++ is > 4 break the timer, otherwise go to next Question
         //If answer is wrong, Add/append to div "Wrong" and deduct time.
+    //Set time to 75
     //Start Timer
             //Subtract from timer
             //Update progress bar and label
@@ -33,9 +42,8 @@ quizAnswers = [2, ]
     //Save name and score to local storage score array
 
     //Reset all variables to default
-
-
-
+}
 
 
 //Make event handler for "onclick" and call the handler function, prompting the first question and starting the timer
+$("#start-quiz").on("click", quizHandler);
